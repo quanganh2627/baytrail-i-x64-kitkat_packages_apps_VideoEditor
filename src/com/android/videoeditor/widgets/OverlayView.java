960687f62367ee14c043db943ef12f6dcdb56d7a
Gu, Wangyi <wangyi.gu@intel.com>
2013-08-19 16:24:59 +0800

    [PORT FROM MAIN][Movie studio]: improve the UX when edit the saved title template
    BZ: 100462
    
    There are two issues are improved in this patch.
    1. there is a saved title in the project, if rotate the phone when edit the
    saved title(choose a new title template), but the new picked title template
    is not updated after rotation.
    2. at the end of issue 1, the user has to pick the new title template after
    rotate the phone if he wants to change the title template, then he saves the
    title, but the view of title in the thumbnail area is not updated.
    
    Category: aosp improvement
    Domain: Video.Media-video editor
    Origin: internal
    Upstream-Candidate: Yes
    
    Change-Id: I83b0f619202944f892246f7b6e4b63dc3fcec04a
    Orig-Change-Id: I925d388cc5c0ee473b5fd76030fca15cfea25b46
    Signed-off-by: Gu, Wangyi <wangyi.gu@intel.com>
    Signed-off-by: Dan Liang <dan.liang@intel.com>

--- src/com/android/videoeditor/widgets/OverlayView.java
+++ src/com/android/videoeditor/widgets/OverlayView.java
@@ -129,6 +129,7 @@ public class OverlayView extends ImageView {
             }
 
             case STATE_OVERLAY: {
+                invalidate();
                 setBackgroundResource(R.drawable.timeline_item_selector);
                 setImageDrawable(null);
                 break;
